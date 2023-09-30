5c734955fe35b8e1b30c205f3a2d406d67623c3d
Kenway <krkeshav123ytb@gmail.com>
2023-09-30 15:13:34 +0530

    stage 6 final

--- app/main.go
+++ app/main.go
@@ -17,21 +17,6 @@ var httpClient = &http.Client{
 	Timeout: 10 * time.Second,
 }
 
-// type ManifestResponse struct {
-// 	SchemaVersion int    `json:"schemaVersion"`
-// 	MediaType     string `json:"mediaType"`
-// 	Config        struct {
-// 		MediaType string `json:"mediaType"`
-// 		Size      int    `json:"size"`
-// 		Digest    string `json:"digest"`
-// 	} `json:"config"`
-// 	Layers []struct {
-// 		MediaType string `json:"mediaType"`
-// 		Size      int    `json:"size"`
-// 		Digest    string `json:"digest"`
-// 	} `json:"layers"`
-// }
-
 type ManifestResponse struct {
 	SchemaVersion int    `json:"schemaVersion"`
 	MediaType     string `json:"mediaType"`
